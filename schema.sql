ad8ec2308262b9fd408ec014f5123230a4044487
benjamin kisenge <benkisenge03@gmail.com>
2022-10-25 16:24:07 +0200

    Add generated always Id

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT,
-    pets_name VARCHAR(250),
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),
     date_of_birth  DATE,
     escape_attempts INT,
     neutered BOOLEAN,
