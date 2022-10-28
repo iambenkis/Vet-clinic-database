2f33f544900e03317388e7b20c0dcce13bbc9dc3
benjamin kisenge <benkisenge03@gmail.com>
2022-10-28 14:40:45 +0200

    Query data

--- queries.sql
+++ queries.sql
@@ -8,3 +8,24 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT detetePoint;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO detetePoint;
+COMMIT;
+
