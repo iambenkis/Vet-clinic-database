6e44397c8de649ff4957b79082e20c53ee02328a
benjamin kisenge <benkisenge03@gmail.com>
2022-10-29 14:58:31 +0200

    Create tables shema

--- schema.sql
+++ schema.sql
@@ -1,12 +1,38 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(250),
     date_of_birth  DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL
 );
--- Add species column
+
 ALTER TABLE animals ADD COLUMN species varchar(100);
+
+-- Add owners table
+CREATE TABLE owners (
+    id BIGSERIAL PRIMARY KEY,
+    full_name VARCHAR(500),
+    age INT
+);
+
+-- Add species table
+CREATE TABLE species (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(200)
+);
+
+ALTER TABLE animals ADD PRIMARY KEY (id);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id BIGINT REFERENCES species(id);
+ALTER TABLE animals ADD owner_id BIGINT REFERENCES owners(id);
+SELECT species.name, COUNT(animals.species_id) FROM animals JOIN species ON species.id = animals.species_id GROUP BY species.name;
+
+SELECT animals.name from animals JOIN owners ON owners.id = animals.owner_id JOIN species on species.id = animals.species_id WHERE animals.species_id = '2' AND animals.owner_id = '2';
+SELECT animals.name from animals JOIN owners ON owners.id = animals.owner_id WHERE animals.escape_attempts = '0' AND animals.owner_id = '5';
+SELECT full_name, COUNT(owner_id) FROM owners JOIN animals on owners.id = animals.owner_id GROUP BY full_name ORDER BY COUNT (owner_id) desc limit 1;
+
+
+
+
